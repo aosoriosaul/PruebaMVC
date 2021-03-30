3005bbc51dabdbcc4a10f6169e786357775c639d
aosoriosaul <aosoriosaul@hotmail.com>
2021-03-30 15:58:11 -0600

    Agregar archivos de proyecto.

---
+++ PruebaMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int ID = 1)
+        {
+            //return "This is the Welcome action method...";
+            ViewData["Mensaje"] = "Hola " + name;
+            ViewData["Intentos"] = ID;
+            return View();
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
